8cf66188ccb1adb2a74b5f52f499a83d321303dc
skmtseiyaku <skmtseiyaku@outlook.com>
2021-04-22 23:08:25 +0900

    dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::io;
+use rand::Rng;
 
 fn main(){
     println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("[Debug] The secret number is: {}", secret_number);
+
     println!("Input your guess:");
 
     let mut guess = String::new();
 
-    let foo = 5; //immutable
-    let mut foo = 5; //mutable
+    //let foo = 5; //immutable
+    //let mut foo = 5; //mutable
 
 
     io::stdin().read_line(&mut guess). //std::io::stdin; & reference;
