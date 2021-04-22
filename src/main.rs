b7db3deb92406ddd07ec2f42a261f2bab2c3dab4
skmtseiyaku <skmtseiyaku@outlook.com>
2021-04-22 22:38:26 +0900

    part1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main(){
+    println!("Guess the number");
+    println!("Input your guess:");
+
+    let mut guess = String::new();
+
+    let foo = 5; //immutable
+    let mut foo = 5; //mutable
+
+
+    io::stdin().read_line(&mut guess). //std::io::stdin; & reference;
+        expect("Error occurred."); // io::Result
+
+    println!("You guessed:{}",guess);
 }
